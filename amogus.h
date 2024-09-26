5880710232b594ad9bce0881b325e3f1904095e2
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-09-26 21:52:18 +0300

    Create amogus.h
    Может когда-то пригодится

---
+++ amogus.h
@@ -0,0 +1,17 @@
+void amogus(){
+    for(int x=0; x<7; ++x){
+    digitalWrite(2, LOW);
+    delay(100);
+    digitalWrite(2, HIGH);
+    delay(200);
+    }
+
+    delay(450);
+
+    for(int x=0; x<3; ++x){
+    digitalWrite(2, LOW);
+    delay(75);
+    digitalWrite(2, HIGH);
+    delay(75);
+    }
+}
