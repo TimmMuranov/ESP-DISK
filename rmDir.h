db54f3abf5be882a854493d67c741dbd189bc0ca
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-10-04 12:33:19 +0300

    Update

---
+++ rmDir.h
@@ -0,0 +1,37 @@
+/*
+Данная функция удаляет num директорий
+из строки dirs. 
+Строка может быть
+в единственной конфигурации: /а/б/с/
+слеш в конце обязателен.
+*/
+
+#include "arduino.h"
+String rmLastDir(String dirs, int num){
+    if (dirs[0] != '/' || dirs[dirs.length()-1] != "/"){
+    	Serial.println ("rmLD - неверный ввод");
+        return dirs;
+    }
+	if (dirs.length() <= 1){
+		Serial.println ("rmLD - вы уже в /");
+        return dirs;
+    }
+//_________________________________________
+  while (num){
+  	if (dirs == "/") return dirs;
+     dirs.remove(dirs.length());
+     int sNum = dirs.lastIndexOf("/");
+     dirs.remove(sNum + 1); //сам '/' остается
+     --num;
+  }
+  return dirs;
+}
+
+/*   
+примеры использования:
+("/", 1) => "/"
+("/a/", 9999) => "/"
+("/a/b/c/", 2) => "/a/”
+("dirrr", 1) => "dirrr"
+
+*/
