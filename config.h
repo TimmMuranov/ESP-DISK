ffc361a42b599f00bf3d4dc1e3d721d5148b485a
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-11-21 21:25:14 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
 const char *ssid = "EspServer"; //имя точки доступа
 const char *password = "12345678"; //пароль точки доступа
-String ServerName = "Servak"; // имя на главной странице
+String serverName = "Servak"; // имя на главной странице
