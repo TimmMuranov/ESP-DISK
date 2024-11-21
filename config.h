33f0027672c519f40b62ce29c40bfbcbb8d480c8
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-11-21 22:37:56 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
-const char *ssid = "EspServer"; //имя точки доступа
+const char *ssid = "EspServer"; //имя точки доступа. 
 //const char *password = "12345678"; //пароль точки доступа. Раскоментить если нужно
-String serverName = "Servak"; // имя на главной странице
+String serverName = "Servak"; // имя на главной странице. Отображается в титле страницы и на сервере
