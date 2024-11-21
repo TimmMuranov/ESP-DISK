79764216c640c708fac479e8ccd41e84508badcc
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-11-21 23:36:24 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -1,3 +1,4 @@
-const char *ssid = "EspServer"; //имя точки доступа. 
-//const char *password = "12345678"; //пароль точки доступа. Раскоментить если нужно
-String serverName = "Servak"; // имя на главной странице. Отображается в титле страницы и на сервере
+const char *ssid = "EspServer"; //имя точки доступа
+//const char *password = "12345678"; //пароль точки доступа
+String serverName = "Servak"; // имя на главной странице
+String filePswd = "123"; // Пароль для редактирования файлов
